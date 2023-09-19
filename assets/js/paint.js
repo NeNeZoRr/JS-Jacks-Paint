0e55a31f021ccc6f22f030c370fb6d074f737263
NeNeZoRr <zorrillalin@gmail.com>
2023-09-19 18:02:33 -0400

    Completed 	modified:   assets/js/paint.js

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -1,71 +1,62 @@
 function configureListeners() {
-    let images = // select img elements  
+  let images = document.querySelectorAll("img");
 
+  // iterate over images and add mouseover event listeners
 
-     for (var i = 0; i < images.length; i++) {        
-        // iterate over images and add mouseover event listeners      
-    } 
+  images.forEach((image) => {
+    image.addEventListener("mouseover", addOpacity);
+    image.addEventListener("mouseout", removeOpacity);
+  });
 }
 
+// Call the function to configure listeners when the page loads
 function addOpacity(event) {
-    // add appropriate CSS class
-    getProductInfo(event.target.id);     
+  if (!this.classList.contains("dim")) {
+    this.classList.add("dim");
+  }
+  getProductInfo(event.target.id);
 }
 
 function removeOpacity(event) {
-     //remove appropriate CSS class
+  if (this.classList.contains("dim")) {
+    this.classList.remove("dim");
+  }
 
-    let element = document.getElementById('color-price');
-        element.textContent = '';
-        
-    let color = document.getElementById('color-name');
-        color.textContent = ''; 
+  let element = document.getElementById("color-price");
+  element.textContent = "";
 
-    event.preventDefault();    
+  let color = document.getElementById("color-name");
+  color.textContent = "";
+
+  event.preventDefault();
 }
 
 function getProductInfo(paintColor) {
-    let price;
-    let colorName;  
-    
-    switch (paintColor) {
-        case 'pn1':           
-            // set variables for price and color name and invoke a function to update the price     
-            break;           
-        case 'pn2':
-            // set variables for price and color name and invoke a function to update the price    
-            break;            
-        case 'pn3':
-            // set variables for price and color name and invoke a function to update the price  
-            break;   
-        case 'pn4':
-            // set variables for price and color name and invoke a function to update the price  
-            break;   
-        case 'pn5':
-            // set variables for price and color name and invoke a function to update the price       
-            break;   
-        case 'pn6':
-            // set variables for price and color name and invoke a function to update the price        
-            break;   
-        case 'pn7':
-            // set variables for price and color name and invoke a function to update the price 
-            break;   
-        case 'pn8':
-            // set variables for price and color name and invoke a function to update the price   
-            break;   
-        case 'pn9':
-            // set variables for price and color name and invoke a function to update the price 
-            break;   
-          default:              
-    }
+  const colorInfo = {
+    pn1: { price: "$19.99", name: "Lime Green" },
+    pn2: { price: "$12.99", name: "Medium Brown" },
+    pn3: { price: "$11.99", name: "Royal Blue" },
+    pn4: { price: "$14.99", name: "Bright Red" },
+    pn5: { price: "$9.99", name: "Solid White" },
+    pn6: { price: "$15.99", name: "Solid Black" },
+    pn7: { price: "$8.99", name: "Medium Blue" },
+    pn8: { price: "$16.99", name: "Light Purple" },
+    pn9: { price: "$17.99", name: "Bright Yellow" },
+  };
 
-    function updatePrice(colorName, price)
-    {       
-        let colorPrice = // select element with corresponding id
-        // display price
-        
-        let color = // select element with corresponding id
-        //display color name
-    }
-    
+  const colorData = colorInfo[paintColor];
+
+  if (colorData) {
+    updatePrice(colorData.name, colorData.price);
+  }
 }
+
+function updatePrice(colorName, price) {
+  let colorPrice = document.getElementById("color-price");
+  colorPrice.textContent = price;
+
+  let color = document.getElementById("color-name");
+  color.textContent = colorName;
+}
+
+configureListeners();
